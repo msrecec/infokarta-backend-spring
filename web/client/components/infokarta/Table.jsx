68e68730a3525ab0fc6b4e38b352939ef19bf855
Marin Tadin <51207820+mtadin@users.noreply.github.com>
2021-03-03 11:13:39 +0100

    table component fixed

--- web/client/components/infokarta/Table.jsx
+++ web/client/components/infokarta/Table.jsx
@@ -1,37 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Table} from 'react-bootstrap'
+import {Table} from 'react-bootstrap';
 
 class TableComponent extends React.Component {
   static propTypes = {
-    // headers: PropTypes.array,
-    items: PropTypes.array
+      items: PropTypes.array
   };
 
   // TODO dodaj keyeve za elemente u listi
   render() {
-    return (
-      <div>
-        <Table striped bordered hover>
-          {/* <thead>
-            <tr>
-              {this.props.headers.map((header) => 
-                <th>{header}</th>
-              )}
-            </tr>
-          </thead> */}
-          <tbody>
-            {this.props.items.map((item) =>
-              <tr>
-                {item.map((field) =>
-                  <td>{field}</td>
-                )}
-              </tr>
-            )}
-          </tbody>
-        </Table>
-      </div>
-    );
+      return (
+          <div>
+              <Table striped bordered hover>
+                  <thead>
+                      <tr>
+                          {this.props.items[0] ?
+                              Object.keys(this.props.items[0]).map((header) =>
+                                  <th>{header}</th>
+                              ) : null}
+                      </tr>
+                  </thead>
+                  <tbody>
+                      {this.props.items.map((item) =>
+                          <tr>
+                              {Object.values(item).map((field) =>
+                                  <td>{field}</td>
+                              )}
+                          </tr>
+                      )}
+                  </tbody>
+              </Table>
+          </div>
+      );
   }
 }
 
