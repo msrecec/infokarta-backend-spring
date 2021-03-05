1010d0b6720b64cebd877f9b9498a17af6604249
Marin Tadin <51207820+mtadin@users.noreply.github.com>
2021-03-05 14:43:36 +0100

    test - pure HTML form

--- web/client/components/infokarta/EditModal.jsx
+++ web/client/components/infokarta/EditModal.jsx
@@ -9,6 +9,23 @@ import {
     hideDynamicModal
 } from "../../actions/infokarta/dynamicModalControl";
 
+const beautifyHeader = (header) => {
+    console.log(header);
+    const regex = /([_])/g;
+    const capitalisedHeader = header.charAt(0).toUpperCase() + header.slice(1);
+    return capitalisedHeader.replaceAll(regex, ' ');
+};
+
+const style = {
+    display: "flex",
+    justifyContent: "space-between"
+};
+
+const modalStyle = {
+    overflow: "auto",
+    maxHeight: "500px"
+};
+
 class BaseModalComponent extends React.Component {
   static propTypes = {
       itemToEdit: PropTypes.object,
@@ -28,7 +45,25 @@ class BaseModalComponent extends React.Component {
               <Modal.Header closeButton>
                   <Modal.Title>Uređivanje stavke</Modal.Title>
               </Modal.Header>
-              <Modal.Body>{this.props.itemToEdit.ime_i_prezime}</Modal.Body>
+              <Modal.Body style={modalStyle}>
+                  {/* <Form> */}
+                  {/* {this.props.itemToEdit ? Object.entries(this.props.itemToEdit).map((entries) =>
+                          <Form.Group controlId={entries[0]}>
+                              <Form.Label>{beautifyHeader(entries[0])}</Form.Label>
+                              <Form.Control as="input" value={typeof entries[1] === undefined ? '' : entries[1]} />
+                          </Form.Group>
+                      ) : null} */}
+                  <form>
+                      {this.props.itemToEdit ? Object.entries(this.props.itemToEdit).map((entries) =>
+                          <div style={style}>
+                              <label>{beautifyHeader(entries[0])}</label>
+                              <input type="text" value={typeof entries[1] === undefined ? "" : entries[1]} />
+                          </div>
+                      ) : null}
+
+                  </form>
+                  {/* </Form> */}
+              </Modal.Body>
               <Modal.Footer>
                   <Button variant="secondary" onClick={this.props.hideModal}>
                   Zatvori
