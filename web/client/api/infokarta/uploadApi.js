856f4edd0d083995079294d790385a2282ed3095
Marin Tadin <marin.tadin2@gmail.com>
2021-04-06 09:58:24 +0200

    added file upload api

---
+++ web/client/api/infokarta/uploadApi.js
@@ -0,0 +1,71 @@
+import axios from "../../libs/ajax";
+
+const Api = {
+    uploadFile: function(entityName, documentType, fileName, file, entityFid) {
+        const url = `http://localhost:8080/mapstore/rest/config/${entityName}/upload/media/${documentType}?entityFid=${entityFid}`;
+
+        const body = {
+            name: fileName,
+            file: file,
+            fk: entityFid
+        };
+
+        const header = {
+            "Content-Type": "multipart/form-data;"
+        };
+
+        return axios.post(
+            url,
+            body,
+            {
+                headers: header
+            })
+            .then(function(response) {
+                console.log(response.data);
+                return response;
+            }).catch(function(error) {
+            /* eslint-disable no-console */
+                console.error(error);
+            });
+    },
+    getFile: function(entityName, documentType, fid, thumbnail) {
+        let url = '';
+        if (thumbnail) {
+            url = `http://localhost:8080/mapstore/rest/config/${entityName}/download/media/${documentType}/${fid}&thumbnail=${thumbnail}`;
+        } else {
+            url = `http://localhost:8080/mapstore/rest/config/${entityName}/download/media/${documentType}/${fid}`;
+        }
+        return axios.get(url)
+            .then(function(response) {
+                console.log(response.data);
+                return response;
+            }).catch(function(error) {
+            /* eslint-disable no-console */
+                console.error(error);
+            });
+    },
+    getMeta: function(entityName, documentType, fid) {
+        const url = `http://localhost:8080/mapstore/rest/config/${entityName}/download/media/${documentType}/meta/${fid}`;
+        return axios.get(url)
+            .then(function(response) {
+                console.log(response.data);
+                return response;
+            }).catch(function(error) {
+            /* eslint-disable no-console */
+                console.error(error);
+            });
+    },
+    getMetaByEntityFid: function(entityName, documentType, entityFid) {
+        const url = `http://localhost:8080/mapstore/rest/config/${entityName}/download/media/${documentType}/meta?entityFid=${entityFid}`;
+        return axios.get(url)
+            .then(function(response) {
+                console.log(response.data);
+                return response;
+            }).catch(function(error) {
+            /* eslint-disable no-console */
+                console.error(error);
+            });
+    }
+};
+
+export default Api;
